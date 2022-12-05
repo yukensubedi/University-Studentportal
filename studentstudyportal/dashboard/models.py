327365529fe5be4eb9e94baf8397605d4b44f187
Ukn Subedi <yukensubedi@gmail.com>
2022-12-06 00:04:21 +0545

    Notes Section created

---
+++ studentstudyportal/dashboard/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Notes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self): #returns the name of notes as per title
+        return self.title
+    
+    class Meta: #avoids plural form in admin pannel for tablename
+        verbose_name = "notes"
+        verbose_name_plural = "notes"
+
+    
+    
---
+++ studentstudyportal/dashboard/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,redirect
+from .forms import *
+from django.contrib import messages
+from django.views import generic
+
+# Create your views here.
+
+def home(request):
+    return render(request,'dashboard/home.html')
+
+
+def notes(request):
+    if request.method == "POST":
+        form = NotesForm(request.POST)
+        if form.is_valid():
+            notes = Notes(user=request.user, title=request.POST['title'],description=request.POST['description'])
+            notes.save()
+        messages.success(request,f"Notes added from {request.user.username} successfully")
+    else:
+        form = NotesForm()
+    
+    notes = Notes.objects.filter(user = request.user)
+    context = {'notes':notes,'form':form}
+    return render(request,'dashboard/notes.html',context)
+
+
+def delete_note(request,pk=None):
+    Notes.objects.get(id=pk).delete()
+    return redirect("notes")
+
+
+class NotesDetailView(generic.DetailView):
+    model = Notes
